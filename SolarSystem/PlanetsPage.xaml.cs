6d3c7e96cc6ac34c69a30573c3f523a04522a4ca
filipsoukal <soukal.filip.03@gmail.com>
2022-05-02 18:32:01 +0200

    Stránky a začátek práce na listu

--- SolarSystem/PlanetsPage.xaml.cs
+++ SolarSystem/PlanetsPage.xaml.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.ObjectModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace SolarSystem {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ShellPage : Shell {
-		private ObservableCollection<SpaceObject> spaceonjectlist;
-		public ShellPage() {
+	public partial class PlanetsPage : ContentPage {
+		public PlanetsPage() {
 			InitializeComponent();
 		}
 	}
